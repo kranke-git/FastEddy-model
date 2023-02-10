3a44b25b354fa08d06d33e9db0a9ff4709cbcc1d
kranke <pgiani@nd.edu>
2023-02-10 11:02:56 -0500

    Modified Makefile and compile for cuda/11.6 compile

--- SRC/FEMAIN/compile.sh
+++ SRC/FEMAIN/compile.sh
@@ -3,10 +3,17 @@
 # Changed Makefile as well to include libraries for netcdf and cuda -- See github for changes
 # Using default cuda module
 
+export LD_LIBRARY_PATH=${INIT_LD_LIBRARY_PATH}
 
-module load cuda/11
-export C_INCLUDE_PATH="/afs/crc/group/EnvStat/build/include:${C_INCLUDE_PATH}"
+module purge
+module load CRC_default
+module load cuda/11.6
+module load intel/19.0
+module -t list
+
+export C_INCLUDE_PATH="/opt/crc/c/cuda/11.6/Linux_x86_64/22.3/cuda/11.6/include:/afs/crc/group/EnvStat/build/include:${C_INCLUDE_PATH}"
 export CPLUS_INCLUDE_PATH="/afs/crc/group/EnvStat/build/include:${CPLUS_INCLUDE_PATH}"
 export NETCDFBASE='/afs/crc/group/EnvStat/build'
 export NETCDF='/afs/crc/group/EnvStat/build'
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/afs/crc/group/EnvStat/build/lib"
 make 
